7b768754351e42691c06fd3ee10857ba56fe9e98
DESKTOP-FLCJD0T\MinJeong <mocaron00@ewhain.net>
2021-01-19 02:29:58 +0900

    javascript 마무리
    동적으로 물건 list 필터링하기

--- src/main.js
+++ src/main.js
@@ -1,28 +1,49 @@
-
-//fetch the items from the JSON file
+// Fetch the items from the JSON file
 function loadItems() {
-   return fetch('data/data.json')
-   .then(response => response.json())  //json으로 변환
-   .then(json => json.items);
+  return fetch('data/data.json')
+    .then(response => response.json())
+    .then(json => json.items);
 }
 
-function displayItems(items){
-    const container = document.querySelector('.items');  //ul 내를
-    container.innerHTML = items.map(item => createHTMLString(item)).join(''); //map 결과 HTML문자열배열을 join해서 하나의 긴 문자열로 저장
+// Update the list with the given items
+function displayItems(items) {
+  const container = document.querySelector('.items');
+  container.innerHTML = items.map(item => createHTMLString(item)).join('');
 }
 
-
-function createHTMLString(item){
-    return `
+// Create HTML list item from the given data item
+function createHTMLString(item) {
+  return `
     <li class="item">
-        <img src="${item.image}" alt="${item.type}" class="item__thumbnail">
+        <img src="${item.image}" alt="${item.type}" class="item__thumbnail" />
         <span class="item__description">${item.gender}, ${item.size}</span>
-  </li>`
-};
-//main
+    </li>
+    `;
+}
+
+function onButtonClick(event, items) {
+  const dataset = event.target.dataset;
+  const key = dataset.key;
+  const value = dataset.value;
+
+  if (key == null || value == null) {
+    return;
+  }
+
+  displayItems(items.filter(item => item[key] === value));
+}
+
+function setEventListeners(items) {
+  const logo = document.querySelector('.logo');
+  const buttons = document.querySelector('.buttons');
+  logo.addEventListener('click', () => displayItems(items));
+  buttons.addEventListener('click', event => onButtonClick(event, items));
+}
+
+// main
 loadItems()
-.then(items => {
+  .then(items => {
     displayItems(items);
-    /*setEventListeners(items)*/
-})
-.catch(console.log);
+    setEventListeners(items);
+  })
+  .catch(console.log);
